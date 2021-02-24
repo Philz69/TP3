f05ac07ae55fcc7d470b4f83e2a6452c1be91816
Philipe Goulet <phil.gou69@hotmail.com>
2021-02-23 19:58:07 -0500

    Added moyenne

--- TP3.py
+++ TP3.py
@@ -1,10 +1,30 @@
-totalNotes = input("Enter the total amount of notes")
-notes = []
+def note_moyenne(notes, totalNotes):
+    moyenne = 0.0;
+    for note in notes:
+        moyenne = moyenne + note/totalNotes
+    return moyenne
+
+def ecart_type(notes, totalNotes):
+    moyenne = note_moyenne(notes, totalNotes)
+    ecartType = 0;
+    for note in notes:
+        ecartType = ((note - moyenne)^2)/2
+    ecartType = ecartType^(1/2)
+
 while True:
+    totalNotes = input("Enter the total amount of notes")
+    if totalNotes.isnumeric():
+        totalNotes = int(totalNotes)
+        break
+
+notes = []
+while len(notes) < totalNotes:
     inputString = input("Entrez une note:")
     try:
         note = float(inputString)
         notes.append(note) 
     except ValueError:
         pass
-    print(notes)
+
+print(notes)
+print(note_moyenne(notes,totalNotes))
